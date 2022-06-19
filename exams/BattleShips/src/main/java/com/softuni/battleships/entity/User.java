4d9804ac52e7823a670f681af19e6104fd11f720
Bobogamen <b_davidov@abv.bg>
2022-06-19 09:48:39 +0300

    exam BattleShips

---
+++ exams/BattleShips/src/main/java/com/softuni/battleships/entity/User.java
@@ -0,0 +1,65 @@
+package com.softuni.battleships.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
