33ed5a11f721dd43ed262cdb570d624e31302752
Bobogamen <b_davidov@abv.bg>
2022-08-08 06:03:44 +0300

    Items added.

--- OnlineShop/src/main/resources/data.sql
+++ OnlineShop/src/main/resources/data.sql
@@ -14,3 +14,52 @@
 # # MODERATOR
 # INSERT INTO onlineshop.users_roles (user_entity_id, roles_id) VALUES (2, 2);
 
+
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (1, 'Classic skin of Jinx.
+#
+# Dimensions:
+# Height 15cm', 5, 'Jinx from League of Legends', 'src\\main\\resources\\static\\images\\items\\Jinx_1.jpg', 9.00, 0, 1);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (2, 'Classic skin of Soraka
+#
+# Dimension
+# Height 15cm.', 0, 'Soraka from League of Legends', 'src\\main\\resources\\static\\images\\items\\Soraka_2.jpg', 9.00, 0, 1);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (3, 'High noon skin of Lucian
+#
+# Dimensions:
+# Height 20cm', 0, 'Lucian from League of Legends', 'src\\main\\resources\\static\\images\\items\\Lucian_3.jpg', 13.00, 0, 1);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (4, 'Hing noon skin of Jhin
+#
+# Dimensions:
+# Height 21cm.', 8, 'Jhin from Leagues of Legends', 'src\\main\\resources\\static\\images\\items\\Jhin from Leagues of Legends_4.jpg', 13.00, 0, 1);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (5, '3D printed Rex
+#
+# Flexible, with hole for key chain.
+#
+# Dimensions;
+# Height 9cm
+# Width 8cm
+# Depth 3cm', 0, 'Rex', 'src\\main\\resources\\static\\images\\items\\Rex_5.jpg', 4.00, 0, 2);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (6, '3D printed Cat
+#
+# Flexible
+#
+# Dimensions;
+# Height 8cm
+# Width 12cm
+# Depth 3cm', 0, 'Cat', 'src\\main\\resources\\static\\images\\items\\Cat_6.jpg', 4.00, 0, 2);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (7, '3D printed model with very cool wings
+#
+# Everypart is flexible
+#
+# Dimensions;
+# Height 12cm
+# Width 19cm
+# Depth 8cm
+#
+# ', 0, 'Dragon with wings', 'src\\main\\resources\\static\\images\\items\\Dragon_7.jpg', 8.00, 0, 2);
+# INSERT INTO onlineshop.items (id, description, discount, name, picture, price, times_ordered, category_id) VALUES (8, 'Dragon Trainer skin of Tristana
+#
+# Dimensions;
+# Height 14cm
+# ', 9, 'Tristana from League of Legends', 'src\\main\\resources\\static\\images\\items\\Tristana_8.jpg', 14.00, 0, 1);
+#
